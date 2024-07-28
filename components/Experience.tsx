3d637514e25865064fdb1f758e54c6350ad637c3
Abdullah462002 <abdul95972@gmail.com>
2024-07-28 23:13:22 +0530

    style change in experience tab

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -13,8 +13,14 @@ const Experience = () => {
           <Button
             key={card.id}
             borderRadius="1.75rem"
-            className="flex text-white border-neutral-200 dark:border-slate-800 p-5"
             duration={Math.floor(Math.random() * 10000) + 10000}
+            style={{
+              background: 'rgb(4,7,29)',
+              backgroundColor:
+                'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
+              borderRadius: `calc(1.75rem* 0.96)`,
+            }}
+            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
           >
             <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
               <img
